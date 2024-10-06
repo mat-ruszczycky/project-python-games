52e2ca38006ea544619b019bbd9b18d88022fa68
Mat Ruszczycky <mat_ruszczycky@apple.com>
2024-10-06 02:20:30 -0700

    refactor: game engine to singleton and corrected module calls

--- cli-game-engine/src/core/game_engine.py
+++ cli-game-engine/src/core/game_engine.py
@@ -5,28 +5,39 @@ from src.io.input_handler import InputHandler
 from src.io.output_handler import OutputHandler
 
 class GameEngine:
+    _instance = None
+   
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(GameEngine, cls).__new__(cls)
+        return cls._instance
+
     def __init__(self, state: GameState, inputHandler: InputHandler, outputHandler: OutputHandler) -> None:
-        self.state = state
+        if not hasattr(self, '_initialized'):
+            self.state = state
+            self.input_handler = inputHandler
+            self.output_handler = outputHandler
+            self._initialized = True
 
     def shouldContinue(self, value: str) -> bool:
         return value.lower() == 'y'
     
     def stop(self) -> None:
         self.state.stop_game()
-        print('Game is stopped.')
+        self.output_handler.display_message("Game is stopped.")
 
     def play(self, gameLoop: callable) -> None:
         self.state.start_game()
-        print("Starting the game...")
+        self.output_handler.display_message("Game is starting...")
 
         while self.state.isPlaying:
             gameLoop()
 
-            userInput = input('Continue to play? (y/n): ').strip().lower()
+            userInput = self.input_handler.get_user_input('Continue playing? (y/n): ')
 
             while userInput not in ['y', 'n']:
-                print("Invalid input. Please enter 'y' to continue or 'n' to stop.")
-                userInput = input('Continue to play? (y/n): ').strip().lower()
+                self.output_handler.display_message("Invalid input. Please enter 'y' to continue or 'n' to stop.")
+                userInput = self.input_handler.get_user_input('Continue playing? (y/n): ')
 
             if not self.shouldContinue(userInput):
                 self.state.stop_game()
