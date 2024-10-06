df704952f941b2eebb0ae1aba5a32fc753908c39
Mat Ruszczycky <mat_ruszczycky@apple.com>
2024-10-06 02:35:59 -0700

    refactor(GameState): class updated to singleton

--- cli-game-engine/src/core/game_state.py
+++ cli-game-engine/src/core/game_state.py
@@ -1,8 +1,17 @@
 # CLI Game Engine
 
 class GameState:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(GameState, cls).__new__(cls)
+        return cls._instance
+
     def __init__(self) -> None:
-        self.isPlaying = False
+        if not hasattr(self, '_initialized'):
+            self.isPlaying = False
+            self._initialized = True  # Prevents re-initialization
 
     def startGame(self) -> None:
         self.isPlaying = True
